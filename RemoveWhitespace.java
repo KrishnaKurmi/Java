c13cfba0991763414f42e6f27a5355963c07d9a2
unknown <hollybuddy.ts2206@gmail.com>
2025-08-05 02:28:59 +0530

    commiting on past day

---
+++ RemoveWhitespace.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RemoveWhitespace {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Replace all whitespace characters using regex
+        String noWhitespace = input.replaceAll("\\s+", "");
+
+        System.out.println("String without whitespaces: " + noWhitespace);
+
+    }
+}
